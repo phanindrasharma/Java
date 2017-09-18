1d9a2d8e51a7cb12c61b4ec40761bc1a10534a9d
ಫಣೀಂದ್ರ ಶರ್ಮ <31012218+phanindrasharma@users.noreply.github.com>
2017-09-18 14:25:34 +0530

    Add files via upload

---
+++ src/strings/Stringcat.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class Stringcat
+{
+
+	 public static void main(String a[])
+	 {
+	        String b = "Phanindra ";
+	        String c = "Sharma";
+	        
+	        
+	        System.out.println(b);
+	        System.out.println(c);
+	        
+	        String d = b+c;
+	        System.out.println(d);
+	        
+	        
+	       // d = b.concat(c);
+	       // System.out.println(d);
+	    }
+}
